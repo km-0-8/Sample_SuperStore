3d0b65bcbaae2dd6bd29b38bb46818ff3eaba044
DESKTOP-1NQ5OF6\coga0082 <kaiya_murakami@co-graph.co.jp>
2025-04-22 14:28:36 +0900

    再修正

--- main_dashbord.py
+++ main_dashbord.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import seaborn as sns
 import numpy as np
-font_prop = fm.FontProperties(fname='fonts/NotoSansJP-Regular.ttf')
+font_prop = fm.FontProperties(fname='fonts/ipaexg.ttf')
 plt.rcParams['font.family'] = font_prop.get_name()
 
 # %%
